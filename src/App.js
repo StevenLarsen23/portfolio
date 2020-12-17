54641ad3a3b12a25884509fe5ef6cbdb0e1cb6e4
Steven Larsen <slarsen0823@gmail.com>
2020-12-16 19:50:48 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,21 @@ function App() {
           <h1>Steven Larsen</h1>
         </div>
         <div className="links">
-          <a href="https://github.com/StevenLarsen23">GitHub</a>
+          <a className='github' href="https://github.com/StevenLarsen23">GitHub</a>
           <br />
-          <a href="https://www.linkedin.com/in/steven-larsen-ab413b200/">
+          <a className='linkedin' href="https://www.linkedin.com/in/steven-larsen-ab413b200/">
             Linkedin
           </a>
-          <a href='https://docs.google.com/document/d/1hmPt5Tghp419HxzsXZQgUgGctHCaPpN3-guGMmNsuR4/edit?usp=sharing'>Resume</a>
+          <a className='resume' href="https://docs.google.com/document/d/1hmPt5Tghp419HxzsXZQgUgGctHCaPpN3-guGMmNsuR4/edit?usp=sharing">
+            Resume
+          </a>
         </div>
       </header>
-      <body className='body'>Body...</body>
+      <main className="body">
+        <div>
+          Body...
+        </div>
+      </main>
     </div>
   );
 }
