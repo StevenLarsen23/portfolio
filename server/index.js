90c984c584ef033f0ee74513801e350f64298110
Steven Larsen <slarsen0823@gmail.com>
2021-01-11 09:30:10 -0700

    commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const { SERVER_PORT } = process.env;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(__dirname + "/../build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
+
+app.listen(SERVER_PORT, () =>
+  console.log(`Server is running on port: ${SERVER_PORT}`)
+);
